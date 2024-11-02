9f483a7ee27bbef4bfba41d3ff5a6d36d1c3d15d
Paul123111 <20102629wit@gmail.com>
2024-11-02 14:17:00 +0000

    Added Trending Today page.

--- movies/src/pages/trendingMoviesPage.js
+++ movies/src/pages/trendingMoviesPage.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { getUpcomingMovies } from "../api/tmdb-api";
+import { getTrendingMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
-//import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
-import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
-const UpcomingMoviesPage = (props) => {
+const TrendingMoviesPage = (props) => {
 
-  const {  data, error, isLoading, isError }  = useQuery('movie', getUpcomingMovies)
+  const {  data, error, isLoading, isError }  = useQuery('trending', getTrendingMovies)
 
   if (isLoading) {
     return <Spinner />
@@ -26,12 +25,12 @@ const UpcomingMoviesPage = (props) => {
 
   return (
     <PageTemplate
-      title="Upcoming Movies"
+      title="Trending Today"
       movies={movies}
       action={(movie) => {
-        return <AddToPlaylistIcon movie={movie} />
+        return <AddToFavoritesIcon movie={movie} />
       }}
     />
   );
 };
-export default UpcomingMoviesPage;
+export default TrendingMoviesPage;
